6a3b94e82c6c49c22f0fc4139996fc7665466170
sinapirani <realsinapirani@gmail.com>
2023-03-16 01:27:14 +0330

    Add bin to package

---
+++ bin/timeParser.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.timeParser = void 0;
+const timeParser = (time, splitSecond) => {
+    const [hours, minutes, secondsAndMS] = time.split(":");
+    const [seconds, milliseconds] = secondsAndMS.split(",");
+    //get total millisecond
+    const totalMS = parseInt(hours) * 60 * 60 * 1000 +
+        parseInt(minutes) * 60 * 1000 +
+        parseInt(seconds) * 1000 +
+        parseInt(milliseconds) +
+        +splitSecond * 1000;
+    //generate new times
+    const newHours = Math.floor(totalMS / (60 * 60 * 1000))
+        .toString()
+        .padStart(2, "0");
+    const newMinutes = Math.floor((totalMS / (60 * 1000)) % 60)
+        .toString()
+        .padStart(2, "0");
+    const newSeconds = Math.floor((totalMS / 1000) % 60)
+        .toString()
+        .padStart(2, "0");
+    const newMilliseconds = (totalMS % 1000).toString().padStart(3, "0");
+    return `${newHours}:${newMinutes}:${newSeconds},${newMilliseconds}`;
+};
+exports.timeParser = timeParser;
